8421c9ddb22e78db60be978ae8bddebaae560000
Fabringio <fabrizio@certiverse.com>
2026-01-06 17:49:39 -0300

    Week 1 Programming Excersie 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        bool playAgain = true;
+        Random rand = new Random();
+
+        while (playAgain)
+        {
+            bool isCorrect = false;
+            int guessCount = 0;
+            int magicNumber = rand.Next(1, 101);
+
+            do
+            {
+                Console.WriteLine("What is your guess?");
+                int userGuess = int.Parse(Console.ReadLine());
+
+                if (userGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                    guessCount++;
+                }
+                else if (userGuess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                    guessCount++;
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    isCorrect = true;
+                }
+            } while (!isCorrect);
+
+            Console.WriteLine($"You took {guessCount} guesses to find the number {magicNumber}.");
+            Console.WriteLine("Do you want to play again? (y/n)");
+            string response = Console.ReadLine().ToLower();
+
+            if (response == "n")
+            {
+                playAgain = false;
+            }
+        }
     }
 }
