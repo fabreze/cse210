856f351624138b3133be1dee70701e62baa42d00
fabreze <fcaballerourquhart@byupathway.edu>
2026-01-06 23:29:23 -0300

    Week 1 Programming Activity Exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+
+        DisplayResult(name, number);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("What is your name?");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("What is your favorite number?");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int squareNumber = number * number;
+        return squareNumber;
+    }
+
+    static void DisplayResult(string name, int number)
+    {
+        int squaredNumber = SquareNumber(number);
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
     }
 }
