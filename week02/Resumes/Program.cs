21bdabdc5363e7844a6be86912b62b7d9554cba2
fabreze <fcaballerourquhart@byupathway.edu>
2026-01-12 23:35:43 -0300

    W02 Learning Activity: Abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Fast Tech";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2010;
+        job1._endYear = 2015;
+
+       Job job2 = new Job();
+        job2._company = "Innova Tech";
+        job2._jobTitle = "Senior Software Engineer";
+        job2._startYear = 2015;
+        job2._endYear = 2020;
+
+        Resume myResume = new Resume();
+        myResume._name = "Fabrizio Caballero";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayResumeDetails();
     }
 }
