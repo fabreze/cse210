e01a64c390030d497dc56b6999648bf7c0d453b0
fabreze <fcaballerourquhart@byupathway.edu>
2026-01-23 22:59:16 -0300

    W03 Learning Activity: Encapsulation

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction1 = new Fraction(3, 4);
+        Console.WriteLine($"Fraction 1: {fraction1.GetFractionString()}");
+        Console.WriteLine($"Decimal Value: {fraction1.GetDecimalValue()}");
+
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine($"Fraction 2: {fraction2.GetFractionString()}");
+        Console.WriteLine($"Decimal Value: {fraction2.GetDecimalValue()}");
+
+        Fraction fraction3 = new Fraction();
+        fraction3.SetTop(2);
+        fraction3.SetBottom(5);
+        Console.WriteLine($"Fraction 3: {fraction3.GetFractionString()}");
+        Console.WriteLine($"Decimal Value: {fraction3.GetDecimalValue()}");
     }
 }
