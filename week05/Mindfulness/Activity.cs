f7fc5f0e99c90fe8bd0e6b1e2de68b5be83e10c5
Fabringio <fabrizio@certiverse.com>
2026-02-06 17:05:25 -0300

    W05 Project: Mindfulness Program Part 1

---
+++ week05/Mindfulness/Activity.cs
@@ -0,0 +1,32 @@
+public class Activity
+{
+    private string _name;
+    private string _description;
+    private int _duration;
+    public Activity()
+    {
+        _name = "";
+        _description = "";
+        _duration = 0;
+    }
+
+    public void DisplayStartingMessage()
+    {
+
+    }
+
+    public void DisplayEndingMessage()
+    {
+
+    }
+    public void ShowSpinner(int seconds)
+    {
+
+    }
+
+    public void ShowCountDown(int seconds)
+    {
+
+    }
+
+}
